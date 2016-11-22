2d4c4b5a741e7e5bcbbc94ed6da125678afa0486
Philip Crotwell <crotwell@seis.sc.edu>
2016-11-22 15:08:32 -0500

    add play button for chord id components, uses ember-audio

--- app/components/seven-id-quiz.js
+++ app/components/seven-id-quiz.js
@@ -1,9 +1,14 @@
 import Ember from 'ember';
+import Audio from 'ember-audio';
 import QM from '../utils/quiz-maker';
 
 export default Ember.Component.extend({
+  audio: Ember.inject.service(),
   mtquiz: Ember.inject.service(),
   clearOnNextKey: false,
+  isLoading: false,
+  font: null,
+  notes: null,
 
   init() {
     this._super(...arguments);
@@ -13,6 +18,18 @@ export default Ember.Component.extend({
     this.set('answer','');
     this.set('right','');
     this.set('correctAnswer', this.get('quiz').get('chordRoman'));
+    this.initSoundFont();
+  },
+  initSoundFont() {
+    // acoustic_grand_piano-mp3.js is a soundfont from
+    // https://github.com/gleitz/midi-js-soundfonts
+    this.get('audio').load('/assets/acoustic_grand_piano-mp3.js').asFont('piano')
+      .then((font) => {
+        this.font = font;
+        // Slicing just so the whole keyboard doesn't show up on the screen
+        this.set('notes', font.get('notes')); //.slice(39, 51));
+        this.set('isLoading', false);
+      });
   },
 
   actions: {
@@ -27,6 +44,27 @@ export default Ember.Component.extend({
         this.set('right', "Hum, maybe you should check that and try again.");
         this.set('clearOnNextKey', true);
       }
+    },
+    playPianoNote(chord) {
+      let playableChord = [];
+      for(let n of chord.notes()) {
+        let note = this.get('font').getNote(n.scientific());
+        if ( ! note) {
+          console.log("can't find note for: "+n.scientific());
+          // try enharmonic
+          note = this.get('font').getNote(n.enharmonics(true)[0].scientific());
+        if ( note) {
+          console.log('  found it as '+n.enharmonics(true)[0].scientific());
+        } else {
+          console.log("not found as enharmonic "+n.enharmonics(true)[0].scientific());
+        }
+}
+        playableChord.push(note);
+      }
+console.log('playable: '+playableChord);
+      let lsound = new Audio.LayeredSound();
+      lsound.set('sounds', playableChord);
+      lsound.play(3.0);
     },
     next() {
       this.set('answer', '');
